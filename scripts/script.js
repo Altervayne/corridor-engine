9b007c81fbdded25fc9a39080737881af982d757
Florian Douay <flooooo85@gmail.com>
2023-09-26 17:54:59 +0200

    finished basic features

--- scripts/script.js
+++ scripts/script.js
@@ -4,23 +4,98 @@
 async function fetchArea(areaName) {
     const areaData = await (await fetch(`public/adventure/areas/${areaName}.json`)).json()
 
+    sessionStorage.setItem("areaData", JSON.stringify(areaData))
+
     return areaData
+}
+
+/* Function to parse the active area's json data from session storage */
+async function getActiveArea() {
+    try {
+        const areaDataString = sessionStorage.getItem("areaData")
+
+        const areaData = JSON.parse(areaDataString)
+
+        return areaData
+    } catch (error) {
+        console.error(`Error parsing areaData from sessionStorage: ${error}`)
+        return null
+    }
 }
 
 /* Function to find the needed scene in the area data */
 function findScene(areaData, id) {
-    const scene = areaData.find((scene) => scene.id === id)
+    console.log(areaData)
+    const scene = areaData.filter((scene) => scene.id === id)
 
-    return scene
+    return scene[0]
 }
 
 /* Function to initialize new scene load */
-function loadScene(pageElements, scene) {
+async function loadScene(pageElements, scene) {
     pageElements.title.textContent = scene.name
     pageElements.description.textContent = scene.text
     pageElements.image.src = scene.image
     pageElements.choices.innerHTML = ""
+
+    console.log(scene)
+
+    for(const choice of scene.choices) {
+        let isDisabled = false
+
+        for(const flag of choice.forbiddenFlags) {
+            if(!isDisabled) {
+                const hasForbiddenFlag = await flagHandler("check", flag)
+
+                if(hasForbiddenFlag) {
+                    isDisabled = true
+                }
+            }
+        }
+        for(const flag of choice.requiresFlags) {
+            if(!isDisabled) {
+                const hasRequiredFlag = await flagHandler("check", flag)
+
+                if(!hasRequiredFlag) {
+                    isDisabled = true
+                }
+            }
+        }
+
+        if(!(choice.isSecret && isDisabled)) {
+            choice.disabled = isDisabled
+
+            const button = await createChoiceButton(choice)
+
+            pageElements.choices.appendChild(button)
+        }
+    }
 }
+
+/* Function to handle clicking a choice */
+async function choiceHandler(choice) {
+    const pageElements = getPageElements()
+    const areaData = await getActiveArea()
+
+    for(const flag of choice.addsFlags) {
+        console.log(flag)
+        const flagAlreadyExists = await flagHandler("check", flag.id)
+        if(!flagAlreadyExists) {
+            await flagHandler("set", flag)
+        }
+    }
+    for(const flag of choice.removesFlags) {
+        const flagAlreadyExists = await flagHandler("check", flag)
+        if(flagAlreadyExists) {
+            await flagHandler("remove", flag)
+        }
+    }
+
+    const newScene = findScene(areaData, choice.id)
+    loadScene(pageElements, newScene)
+}
+
+
 
 /* Function to initialize the landing page */
 async function pageInit(pageElements) {
@@ -29,7 +104,8 @@ async function pageInit(pageElements) {
     const gameTitleElement = document.getElementById("gameTitle")
     gameTitleElement.textContent = metadata.title
 
-    const areaData = await fetchArea(metadata.startingArea)
+    await fetchArea(metadata.startingArea)
+    const areaData = await getActiveArea()
     const firstScene = findScene(areaData, metadata.startingScene)
 
     loadScene(pageElements, firstScene)
@@ -43,6 +119,67 @@ async function pageInit(pageElements) {
         loadingScreen.style.display = "none";
     }, 500);
 }
+
+
+
+/* Function to handle flags */
+async function flagHandler(action, flag) {
+    if(action === "set") {
+        const newFlag = {
+            type: flag.type,
+            id: flag.id,
+            name: flag.name,
+            icon: flag.icon
+        }
+
+        const flagJSON = JSON.stringify(newFlag)
+
+        try {
+            sessionStorage.setItem(newFlag.id, flagJSON)
+        }
+        catch(error) {
+            console.error(`Error setting flag "${flag.id}": ${error}`)
+        }
+    }
+
+    if(action === "check") {
+        try {
+            const flagValue = sessionStorage.getItem(flag)
+        
+            if(flagValue) {
+                return true
+            } else {
+                return false
+            }
+        }
+        catch (error) {
+            console.error(`Error checking flag "${flag}": ${error}`)
+
+            return false
+        }
+    }
+
+    if(action === "remove") {
+        try {
+            sessionStorage.removeItem(flag)
+        
+            return flagValue !== null && flagValue !== undefined      
+        }
+        catch (error) {
+            console.error(`Error removing flag "${flag}": ${error}`)
+
+            return false
+        }
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -64,7 +201,7 @@ function getPageElements() {
 }
 
 /* Function to create a choice button */
-function createChoiceButton(choice) {
+async function createChoiceButton(choice) {
     const button = document.createElement("button")
     button.classList.add("scene__choices__button")
     button.textContent = choice.title
@@ -75,9 +212,18 @@ function createChoiceButton(choice) {
         return button
     }
 
-    button.addEventListener("click", loadScene(choice.id))
+    button.addEventListener("click", () => choiceHandler(choice))
     return button
 }
+
+
+
+
+
+
+
+
+
 
 
 
@@ -88,6 +234,3 @@ const pageElements = getPageElements()
 
 /* Initializing page with base metadata info */
 pageInit(pageElements)
-
-/* Initializing page with starting area */
-fetchArea()
