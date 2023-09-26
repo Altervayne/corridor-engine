b3caec28295f2b244786b1b6cb6ad4ffc2c58b0a
Florian Douay <flooooo85@gmail.com>
2023-09-26 15:57:19 +0200

    set up a loading screen

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+/* ------------------------- Data Handling Functions Block ------------------------- */
+
 /* Function to fetch an area's json data */
 async function fetchArea(areaName) {
     const areaData = await (await fetch(`public/adventure/areas/${areaName}.json`)).json()
@@ -5,6 +7,48 @@ async function fetchArea(areaName) {
     return areaData
 }
 
+/* Function to find the needed scene in the area data */
+function findScene(areaData, id) {
+    const scene = areaData.find((scene) => scene.id === id)
+
+    return scene
+}
+
+/* Function to initialize new scene load */
+function loadScene(pageElements, scene) {
+    pageElements.title.textContent = scene.name
+    pageElements.description.textContent = scene.text
+    pageElements.image.src = scene.image
+    pageElements.choices.innerHTML = ""
+}
+
+/* Function to initialize the landing page */
+async function pageInit(pageElements) {
+    const metadata = await (await fetch("public/adventure/metadata.json")).json()
+
+    const gameTitleElement = document.getElementById("gameTitle")
+    gameTitleElement.textContent = metadata.title
+
+    const areaData = await fetchArea(metadata.startingArea)
+    const firstScene = findScene(areaData, metadata.startingScene)
+
+    loadScene(pageElements, firstScene)
+
+
+    /* Handling first loading screen */
+    const loadingScreen = document.getElementById("loadingScreen")
+    loadingScreen.style.opacity = "0"
+
+    setTimeout(function() {
+        loadingScreen.style.display = "none";
+    }, 500);
+}
+
+
+
+/* ------------------------- Elements Creation Functions Block ------------------------- */
+
+/* Function to fetch the page's elements and return them in an object */
 function getPageElements() {
     const sceneTitleElement = document.getElementById("sceneTitle")
     const sceneDescriptionElement = document.getElementById("sceneDescription")
@@ -19,20 +63,31 @@ function getPageElements() {
     }
 }
 
-/* Function to initialize the landing page */
-async function pageInit() {
-    const metadata = await (await fetch("public/adventure/metadata.json")).json()
+/* Function to create a choice button */
+function createChoiceButton(choice) {
+    const button = document.createElement("button")
+    button.classList.add("scene__choices__button")
+    button.textContent = choice.title
 
-    const gameTitleElement = document.getElementById("gameTitle")
-    gameTitleElement.textContent = metadata.title
+    if(choice.disabled) {
+        button.classList.add("scene__choices__button__disabled")
+
+        return button
+    }
+
+    button.addEventListener("click", loadScene(choice.id))
+    return button
 }
 
 
 
 /* ------------------------- Running Functions Block ------------------------- */
 
-/* Initializing page with base metadata info */
-pageInit()
-
 /* Initializing an object containing the game's HTML elements */
 const pageElements = getPageElements()
+
+/* Initializing page with base metadata info */
+pageInit(pageElements)
+
+/* Initializing page with starting area */
+fetchArea()
